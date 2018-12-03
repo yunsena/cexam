f87a9698987fd6ecf19a17ea638f797d7c1417f6
unknown <tpsk3374@gmail.com>
2018-12-03 10:22:41 +0900

    test

---
+++ simple-echo/server.cc
@@ -0,0 +1,18 @@
+#include <arpa/inet.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <unistd.h>
+
+#include <iostream>
+
+using namespace std;
+
+int
+main(int argc, char **argv)
+{
+
+    cout << "Hello World" <<endl;
+}
